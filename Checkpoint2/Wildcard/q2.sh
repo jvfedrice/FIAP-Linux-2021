4438bda164b47b64f031f77fa145a57f6c2a6205
xibataa <81631490+xibataa@users.noreply.github.com>
2021-05-20 20:19:39 -0400

    Create q2.sh

---
+++ Checkpoint2/Wildcard/q2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "cd /home/demo | cp * "file" /demo/work | ls /demo/work"
+resposta="Todos os arquivos contendo "file" foram copiados para dentro de /work"
+echo "A resposta correta é $resposta" 
