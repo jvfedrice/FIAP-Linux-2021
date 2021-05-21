be3e35e6c84a685df61ea70c5fc536cfe2057506
xibataa <81631490+xibataa@users.noreply.github.com>
2021-05-20 20:06:00 -0400

    Create q6.sh

---
+++ Checkpoint2/Intro2/q6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "cd /work | cp /home/bigfile2 /demo/scripts/"
+resposta="O diretório atual é work e o arquivo bigfile2 foi movido para a pasta scripts"
+echo "A resposta correta é $resposta" 
